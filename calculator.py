4a196b57cc467e685054be3b04f12f92f4181e36
Eduarda Real Ferreira <dudarealferreira07@gmail.com>
2024-10-01 20:13:14 -0300

    commit

---
+++ calculator.py
@@ -0,0 +1,14 @@
+class Calculadora:
+    def somar(num1, num2):
+        return int(num1) + int(num2)
+    
+    def maiorNumero(num1, num2):
+        if num1 == num2:
+            return 'Os dois números são iguais'
+        if num1 > num2:
+            return f'O maior número é {num1}'
+        else:
+            return f'O maior número é {num2}'
+        
+    def diferenca(numero1, numero2):
+       return abs(int(numero1) - int(numero2))
